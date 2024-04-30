ce7994a9f32d6c50d5d1d3418629ed6d5eea8967
-akku- <151517253+akku1139@users.noreply.github.com>
2024-04-30 03:05:42 +0000

    完成

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AutoPubBot
-Implement https://github.com/Vedinsoh/discord-auto-publisher in Discord.py 
+
+https://github.com/Vedinsoh/discord-auto-publisher
+
+## Required Permission
+
+76800
+
+- Read Messages/View Channels
+- Send Messages
+- Manage Messages
+- Read Message History
---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+
+client = discord.Client(
+  intents = discord.Intents.default()
+)
+
+@client.event
+async def on_message(m: discord.Message):
+  await m.publish()
+
+if __name__ == "__main__":
+  import os
+  TOKEN = os.environ.get("DISCORD_TOKEN")
+
+  if TOKEN is None:
+    print("Set the token in the environment variable DISCORD_TOKEN.")
+    exit(1)
+
+  client.run(TOKEN)
